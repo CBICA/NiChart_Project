f12c285de364956dd710b0e9ba20e191266ac36b
Spiros Maggioros <spirosastro@gmail.com>
2024-10-08 18:52:51 +0300

    Fixed pre-commit for workflow/workflows

---
+++ src/workflow/workflows/w_sMRI/call_snakefile.py
@@ -0,0 +1,51 @@
+import argparse
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--run_dir", help="Provide the path to snakefile", required=True
+    )
+    parser.add_argument(
+        "--dset_name", help="Provide a name for your dataset", required=True
+    )
+    parser.add_argument(
+        "--input_rois", help="Provide input csv name with ROIs", required=True
+    )
+    parser.add_argument(
+        "--input_demog",
+        help="Provide input csv name with demographic info",
+        required=True,
+    )
+    parser.add_argument(
+        "--dir_output", help="Provide output folder name", required=True
+    )
+
+    options = parser.parse_args()
+
+    # Create out dir
+    if not os.path.exists(options.dir_output):
+        os.makedirs(options.dir_output)
+
+    # Change path
+    os.chdir(options.run_dir)
+
+    # Run workflow
+    print("Running: snakemake")
+
+    # cmd = "snakemake -np"
+    # cmd = cmd + " --config dset_name=" + options.dset_name
+    # cmd = cmd + " input_rois=" + options.input_rois
+    # cmd = cmd + " input_demog=" + options.input_demog
+    # cmd = cmd + " dir_output=" + options.dir_output
+
+    cmd = "snakemake "
+    cmd = cmd + " --config dset_name=" + options.dset_name
+    cmd = cmd + " input_rois=" + options.input_rois
+    cmd = cmd + " input_demog=" + options.input_demog
+    cmd = cmd + " dir_output=" + options.dir_output
+    cmd = cmd + " --cores 1"
+
+    print("Running cmd: " + cmd)
+
+    os.system(cmd)
--- src/workflow/workflows/w_sMRI/workflow_mlscores.py
+++ src/workflow/workflows/w_sMRI/workflow_mlscores.py
@@ -1,14 +1,14 @@
-## Import packages
-import pandas as pd
-import json
-import os
-import utils_wsmri as utilw
+# Import packages
 import argparse
+import os
+from typing import Any
+
+import utils_wsmri as utilw
 
 dict_config = {
     "dset_name": "",
     "input_rois": "",
-    "input_demog": "",    
+    "input_demog": "",
     "dir_output": "",
     "list_ROIs_all": "MUSE/list_MUSE_all.csv",
     "list_ROIs_single": "MUSE/list_MUSE_single.csv",
@@ -24,12 +24,13 @@ dict_config = {
     "model_SPARE-Obesity": "models/vISTAG1/SPARE/combined_DLMUSE_raw_COMBAT_SPARE-Obesity_Model.pkl.gz",
     "model_SPARE-Smoking": "models/vISTAG1/SPARE/combined_DLMUSE_raw_COMBAT_SPARE-Smoking_Model.pkl.gz",
     "SPARE_types": ["AD", "Age"],
-    "seg_types": ["DLMUSE"]
+    "seg_types": ["DLMUSE"],
 }
 
-def run_workflow(root_dir, dict_config):
 
-    bdir = os.path.join(root_dir, 'src', 'workflow')
+def run_workflow(root_dir: Any, dict_config: Any) -> None:
+
+    bdir = os.path.join(root_dir, "src", "workflow")
 
     # Read dict_config vars and lists
     dset_name = dict_config["dset_name"]
@@ -42,94 +43,95 @@ def run_workflow(root_dir, dict_config):
     rois_primary = os.path.join(bdir, dict_config["list_ROIs_primary"])
     spare_types = dict_config["SPARE_types"]
     model_combat = os.path.join(bdir, dict_config["model_combat"])
-    seg_types = dict_config["seg_types"]
+    # seg_types = dict_config["seg_types"]
     corr_type = dict_config["corr_type"]
 
-    ## Set output file name
-    OUT_FILE = f"{dir_output}/{dset_name}_DLMUSE+MLScores.csv"
+    # Set output file name
+    # OUT_FILE = f"{dir_output}/{dset_name}_DLMUSE+MLScores.csv"
 
     print(dict_config)
 
     # Rename MUSE roi indices to roi codes
-    out_dir = os.path.join(dir_output, 'working_dir')
+    out_dir = os.path.join(dir_output, "working_dir")
     f_raw = os.path.join(out_dir, f"{dset_name}_raw.csv")
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    utilw.rename_df_columns(input_rois, list_rois, 'Index', 'Code', f_raw)
+    utilw.rename_df_columns(input_rois, list_rois, "Index", "Code", f_raw)
 
     # Normalize ROIs. Values are scaled either by a constant factor (NormICV) or 100 (PercICV)
-    icv_var = 'MUSE_702'
-    exclude_vars = 'MRID'
-    suffix = 'NONE'
+    icv_var = "MUSE_702"
+    exclude_vars = "MRID"
+    suffix = "NONE"
     f_corr = os.path.join(out_dir, f"{dset_name}_{corr_type}.csv")
     utilw.corr_icv(f_raw, corr_type, icv_var, exclude_vars, suffix, f_corr)
 
-    #Merge covars to ROIs
-    key_var = 'MRID'
-    f_comb = os.path.join(out_dir, f'{dset_name}_comb.csv')
+    # Merge covars to ROIs
+    key_var = "MRID"
+    f_comb = os.path.join(out_dir, f"{dset_name}_comb.csv")
     utilw.merge_dataframes(input_demog, f_raw, key_var, f_comb)
 
     # Select variables for harmonization
-    dict_csv=os.path.join('src', 'workflow', rois_single)
-    dict_var = 'Code'
-    covars = 'MRID,Age,Sex,SITE,MUSE_702'
-    f_sel = os.path.join(out_dir, f'{dset_name}_sel.csv')
+    dict_csv = os.path.join("src", "workflow", rois_single)
+    dict_var = "Code"
+    covars = "MRID,Age,Sex,SITE,MUSE_702"
+    f_sel = os.path.join(out_dir, f"{dset_name}_sel.csv")
     utilw.select_vars(f_comb, dict_csv, dict_var, covars, f_sel)
 
     # Check that sample has age range consistent with the model
-    var_name='Age'
-    min_val='50'
-    max_val='95'
-    f_filt = os.path.join(out_dir, f'{dset_name}_filt.csv')
+    var_name = "Age"
+    min_val = 50
+    max_val = 95
+    f_filt = os.path.join(out_dir, f"{dset_name}_filt.csv")
     utilw.filter_num_var(f_sel, var_name, min_val, max_val, f_filt)
 
     # Apply combat
-    mdl = os.path.join('src', 'workflow', model_combat)
-    f_combat1 = os.path.join(out_dir, f'{dset_name}_COMBAT_single.csv')
-    utilw.apply_combat(f_filt, mdl, 'MRID', '_HARM', f_combat1)
+    mdl = os.path.join("src", "workflow", model_combat)
+    f_combat1 = os.path.join(out_dir, f"{dset_name}_COMBAT_single.csv")
+    utilw.apply_combat(f_filt, mdl, "MRID", "_HARM", f_combat1)
 
     # Calculate derived ROIs from harmonized data
-    in_dict = os.path.join('src', 'workflow', derived_rois)
-    key_var='MRID',
-    roi_prefix='MUSE_'
-    f_combat2 = os.path.join(out_dir, f'{dset_name}_COMBAT_all.csv')
+    in_dict = os.path.join("src", "workflow", derived_rois)
+    key_var = "MRID"
+    # roi_prefix = "MUSE_"
+    f_combat2 = os.path.join(out_dir, f"{dset_name}_COMBAT_all.csv")
     utilw.combine_rois(f_combat1, in_dict, f_combat2)
 
     # Merge covars to ROIs
-    key_var = 'MRID'
-    f_combat3 = os.path.join(out_dir, f'{dset_name}_COMBAT_withcovar.csv')
+    key_var = "MRID"
+    f_combat3 = os.path.join(out_dir, f"{dset_name}_COMBAT_withcovar.csv")
     utilw.merge_dataframes(input_demog, f_combat2, key_var, f_combat3)
 
     # Select variables for harmonization
-    dict_var = 'Code'
-    covars ='MRID,Age,Sex,DLICV'
-    f_combat4 = os.path.join(out_dir, f'{dset_name}_COMBAT.csv')
+    dict_var = "Code"
+    covars = "MRID,Age,Sex,DLICV"
+    f_combat4 = os.path.join(out_dir, f"{dset_name}_COMBAT.csv")
     utilw.select_vars(f_combat3, dict_csv, dict_var, covars, f_combat4)
 
     # spare apply
     list_spare = []
     for SPARETYPE in spare_types:
-        mdl = os.path.join(bdir, dict_config[f'model_SPARE-{SPARETYPE}'])
-        f_spare = os.path.join(out_dir, f'{dset_name}_SPARE_{SPARETYPE}.csv')
+        mdl = os.path.join(bdir, dict_config[f"model_SPARE-{SPARETYPE}"])
+        f_spare = os.path.join(out_dir, f"{dset_name}_SPARE_{SPARETYPE}.csv")
         utilw.apply_spare(f_combat4, mdl, SPARETYPE, f_spare)
         list_spare.append(f_spare)
 
     # merge spare
-    f_spares = os.path.join(out_dir, f'{dset_name}_SPARE-ALL.csv')
-    utilw.merge_dataframes_multi(f_spares, 'MRID', list_spare)
+    f_spares = os.path.join(out_dir, f"{dset_name}_SPARE-ALL.csv")
+    utilw.merge_dataframes_multi(f_spares, "MRID", list_spare)
 
     # Merge all
-    f_all = os.path.join(dir_output, f'{dset_name}_DLMUSE+MLScores.csv')
-    utilw.combine_all(f_all, [input_demog, rois_primary, f_raw, f_corr, f_combat2, f_spares])
+    f_all = os.path.join(dir_output, f"{dset_name}_DLMUSE+MLScores.csv")
+    utilw.combine_all(
+        f_all, [input_demog, rois_primary, f_raw, f_corr, f_combat2, f_spares]
+    )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--root_dir", help="Provide the path to root for the script", required=True
     )
-    parser.add_argument(
-        "--run_dir", help="Provide the path to script", required=True
-    )
+    parser.add_argument("--run_dir", help="Provide the path to script", required=True)
     parser.add_argument(
         "--dset_name", help="Provide a name for your dataset", required=True
     )
@@ -155,15 +157,13 @@ if __name__ == "__main__":
     os.chdir(options.run_dir)
 
     # Update dict_config file
-    dict_config['dset_name'] = options.dset_name
-    dict_config['input_rois'] = options.input_rois
-    dict_config['input_demog'] = options.input_demog
-    dict_config['dir_output'] = options.dir_output
+    dict_config["dset_name"] = options.dset_name
+    dict_config["input_rois"] = options.input_rois
+    dict_config["input_demog"] = options.input_demog
+    dict_config["dir_output"] = options.dir_output
 
     # Run workflow
-    print(f"Running: wsmri")
+    print("Running: wsmri")
     run_workflow(options.root_dir, dict_config)
 
     print("Workflow complete! Output file:", options.dir_output)
-
-
