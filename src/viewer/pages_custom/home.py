05ce1053a5abc33527658b30367866d2d5bccd7b
Alexander Getka <Alexander.Getka@pennmedicine.upenn.edu>
2024-10-31 18:31:39 -0400

    home-page based cloud setting

--- src/viewer/pages_custom/home.py
+++ src/viewer/pages_custom/home.py
@@ -2,6 +2,23 @@ import os
 
 import pandas as pd
 import streamlit as st
+import argparse
+
+parser = argparse.ArgumentParser(description='NiChart Application Server')
+
+parser.add_argument('--cloud', action='store_true', default=False,
+                    help="If passed, set the session type to cloud")
+try:
+    args = parser.parse_args()
+except SystemExit as e:
+    # This exception will be raised if --help or invalid command line arguments
+    # are used. Currently streamlit prevents the program from exiting normally
+    # so we have to do a hard exit.
+    os._exit(e.code)
+
+if args.cloud:
+    st.session_state.app_type = "CLOUD"
+
 
 # Initiate Session State Values
 if "instantiated" not in st.session_state:
