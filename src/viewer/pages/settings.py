779f4710d1df0a215bfa4dc51ac56be6195c5be0
gurayerus <gurayerus@gmail.com>
2025-06-30 17:53:15 -0400

    Edits

--- src/viewer/pages/settings.py
+++ src/viewer/pages/settings.py
@@ -28,42 +28,42 @@ def panel_resources_path():
     """
     Panel for selecting resource directories
     """
-    with st.container(border=True):
-        sel_res = st.pills(
-            "Select resource type",
-            ["process definitions", "roi lists"],
-            selection_mode="single",
-            default=None,
-            label_visibility="collapsed",
+    sel_res = sac.tabs(
+        items=[
+            sac.TabsItem(label='Process Definitions'),
+            sac.TabsItem(label='ROI Lists'),
+        ],
+        size='lg',
+        align='left'
+    )
+
+    if sel_res is None:
+        return
+    
+    if sel_res == "process definitions":
+        out_dir = st.session_state.paths["proc_def"]
+
+        # Browse output folder
+        if st.button('Browse path'):
+            sel_dir = utilio.browse_folder(out_dir)
+            utilss.update_out_dir(sel_dir)
+            st.rerun()
+
+        # Enter output folder
+        sel_dir = st.text_input(
+            'Enter path',
+            value=out_dir,
+            # label_visibility='collapsed',
         )
+        if sel_dir != out_dir:
+            utilss.update_out_dir(sel_dir)
+            st.rerun()
 
-        if sel_res is None:
-            return
-        
-        if sel_res == "process definitions":
-            out_dir = st.session_state.paths["proc_def"]
-
-            # Browse output folder
-            if st.button('Browse path'):
-                sel_dir = utilio.browse_folder(out_dir)
-                utilss.update_out_dir(sel_dir)
-                st.rerun()
-
-            # Enter output folder
-            sel_dir = st.text_input(
-                'Enter path',
-                value=out_dir,
-                # label_visibility='collapsed',
+        if st.session_state.flags["out_dir"]:
+            st.success(
+                f"Output directory: {st.session_state.paths['out_dir']}",
+                icon=":material/thumb_up:",
             )
-            if sel_dir != out_dir:
-                utilss.update_out_dir(sel_dir)
-                st.rerun()
-
-            if st.session_state.flags["out_dir"]:
-                st.success(
-                    f"Output directory: {st.session_state.paths['out_dir']}",
-                    icon=":material/thumb_up:",
-                )
 
             #utildoc.util_help_dialog(utildoc.title_out, utildoc.def_out)
 
@@ -71,21 +71,20 @@ def panel_misc() -> None:
     """
     Panel for setting various parameters
     """
-    with st.container(border=True):
-
-        is_cloud_mode = st.session_state.app_type == "cloud"
-        if st.checkbox("Switch to cloud?", value=is_cloud_mode):
-            st.session_state.app_type = "cloud"
-        else:
-            st.session_state.app_type = "desktop"
+    is_cloud_mode = st.session_state.app_type == "cloud"
+    if st.checkbox("Switch to cloud?", value=is_cloud_mode):
+        st.session_state.app_type = "cloud"
+    else:
+        st.session_state.app_type = "desktop"
 
 def panel_debug_options():
-    sel_debug = st.pills(
-        "Select Debug Options",
-        ["Session State", "Output Files"],
-        selection_mode="single",
-        default=None,
-        label_visibility="collapsed",
+    sel_debug = sac.tabs(
+        items=[
+            sac.TabsItem(label='Session State'),
+            sac.TabsItem(label='Output Files'),
+        ],
+        size='lg',
+        align='left'
     )
 
     if sel_debug == "Session State":
@@ -119,17 +118,21 @@ tab = sac.tabs(
 )
 
 if tab == 'Paths':
-    panel_resources_path()
-    panel_models_path()
+    with st.container(border=True):
+        panel_resources_path()
+        panel_models_path()
 
 elif tab == 'Plot Colors':
-    panel_plot_colors()
+    with st.container(border=True):
+        panel_plot_colors()
 
 elif tab == 'Debug':
-    panel_debug_options()
+    with st.container(border=True):
+        panel_debug_options()
 
 elif tab == 'Misc':
-    panel_misc()
+    with st.container(border=True):
+        panel_misc()
 
 # Show session state vars
 if st.session_state.mode == 'debug':
