acef1932b39d0ac2e53d3b0e12f065a7fce06a29
Alexander Getka <alexgetka@gmail.com>
2025-07-22 15:19:45 -0400

    command fix

--- src/viewer/utils/utils_toolloader.py
+++ src/viewer/utils/utils_toolloader.py
@@ -451,7 +451,7 @@ def submit_and_run_job_sync(
                 if os.WEXITSTATUS(returncode) > 0:
                     print(f"DEBUG: Post-job sync failed, retrying appropriately for single-file.")
                     log.error(f"Post job sync failed for job {job_id}. Possibly due to single-file, retrying with applicable command.")
-                    sf_cmd = f"aws cp s3://cbica-nichart-io/{mount_path} {mount_path}"
+                    sf_cmd = f"aws s3 cp s3://cbica-nichart-io/{mount_path} {mount_path}"
                     sf_returncode = os.system(sf_cmd)
                     if os.WEXITSTATUS(sf_returncode) > 0:
                         log.error(f"Single-file sync also failed. Sync is uncompletable.")
