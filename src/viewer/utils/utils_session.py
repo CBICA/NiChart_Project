264249bf9b9fb016d66a43dfe8184e8799710119
Alexander Getka <alexgetka@gmail.com>
2025-11-05 12:26:27 -0500

    init sesh

--- src/viewer/utils/utils_session.py
+++ src/viewer/utils/utils_session.py
@@ -105,6 +105,8 @@ def init_session_vars():
     ]
     st.session_state.pipeline_categories = utiltl.overall_pipeline_category_listing()
     st.session_state.pipeline_requirements = utiltl.overall_pipeline_requirements_listing()
+    st.session_state.harmonizable_pipelines = utiltl.pipeline_categories['harmonized']
+    st.session_state.do_harmonize = False
 
     st.session_state.list_mods = ["T1", "T2", "FL", "DTI", "fMRI"]
     st.session_state.params = {
