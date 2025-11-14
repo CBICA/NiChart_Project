c0777311fc60b9f5a1e322167d91e963c28bb45b
Alexander Getka <alexgetka@gmail.com>
2025-11-14 11:21:58 -0500

    initial run-pipeline view

--- src/viewer/pages/nichart_run_pipeline.py
+++ src/viewer/pages/nichart_run_pipeline.py
@@ -10,8 +10,11 @@ import utils.utils_processes as utilprc
 import utils.utils_session as utilses
 import utils.utils_data_view as utildv
 from utils.utils_styles import inject_global_css
+import utils.utils_toolloader as utiltl
+import utils.utils_stlogbox as stlogbox
 
 from streamlit_image_select import image_select
+import stqdm
 import re
 from utils.utils_logger import setup_logger
 
@@ -73,7 +76,6 @@ def select_pipeline():
 
     pipelines = st.session_state.pipelines
     pnames = pipelines.Name.tolist()
-
     sel_opt = sac.chip(
         pnames,
         label='', index=0, align='left',
@@ -83,6 +85,67 @@ def select_pipeline():
 
     show_description(sel_opt.lower())
 
+def pipeline_runner_menu():
+    sel_method = st.session_state.sel_pipeline
+    st.success(f'Selected pipeline: {sel_method}')
+    harmonize = False
+    harmonizable = ['spare-ad', 'spare-ba', 'dlmuse', 'dlmuse-dlwmls', 'spare-smoking', 'spare-hypertension', 'spare-obesity', 'spare-diabetes']
+    if sel_method in harmonizable:
+        harmonize = st.checkbox("Harmonize to reference data? (Requires >= 30 scans)")
+    ## TODO: Retrieve dynamically/match between front end and toolloader code
+    ## This a nice and simple placeholder for now
+    
+    pipeline_to_run = tl.get_pipeline_id_by_name(sel_method, harmonized=harmonize)
+
+    if pipeline_to_run is None:
+        st.error("The currently selected pipeline doesn't have an associated tool configuration. Please submit a bug report!")
+        return
+    skip_steps_when_possible = True
+    skip_steps_when_possible = st.checkbox("Accelerate pipeline via caching? (Uncheck to force re-runs)", value=True)
+    alert_placeholder = st.empty()
+    if st.button("Run pipeline"):
+        alert_placeholder.info(f"The pipeline {pipeline_to_run} is running. Please do not navigate away from this page.")
+        pipeline_progress_bar = stqdm(total=2, desc="Submitting pipeline...", position=0)
+        #process_progress_bar = stqdm(total=2, desc="Waiting...", position=0)
+        process_progress_bar = None
+        process_status_box = st.status("Submitting pipeline step...", expanded=True)
+        #pipeline_progress_bar_slot = st.empty()
+        #process_progress_bar_slot = st.empty()
+        with st.container():
+            st.subheader("Pipeline Logs")
+            with st.expander("View all pipeline logs"):
+                with st.container():
+                    log_committed_box = st.empty()
+            with st.expander("View current step live logs"):
+                with st.container():
+                    log_live_box = st.empty()
+
+
+        log = stlogbox.StreamlitJobLogger(log_committed_box, log_live_box)
+        execution_mode = 'local'
+        if st.session_state.has_cloud_session:
+            execution_mode = 'cloud'
+        local_path_remapping = {}
+        data_dir_locally = st.session_state.paths["out_dir"]
+        data_dir_on_host = st.session_state.paths["host_out_dir"]
+        if data_dir_on_host is not None:
+            local_path_remapping[data_dir_locally] = data_dir_on_host
+        result = utiltl.run_pipeline(
+            pipeline_id=pipeline_to_run, ##TODO EDIT THIS
+            global_vars={"STUDY": st.session_state.paths["project"]},
+            execution_mode=execution_mode,
+            pipeline_progress_bar=pipeline_progress_bar,
+            process_progress_bar=process_progress_bar,
+            process_status_box=process_status_box,
+            log=log,
+            metadata_location=os.path.join(st.session_state.paths["project"], "metadata.json"),
+            reuse_cached_steps=skip_steps_when_possible,
+            local_path_remapping=local_path_remapping
+        )
+
+        alert_placeholder.success(f"Pipeline {pipeline_to_run} finished successfully.")
+
+    pass
 
 def pipeline_menu():
     cols = st.columns([10,1,10])
@@ -92,7 +155,8 @@ def pipeline_menu():
 
     with cols[0]:
         select_pipeline()
-    # with cols[2]:
+    with cols[1]:
+        pipeline_runner_menu()
 
 #################################
 ## Main
