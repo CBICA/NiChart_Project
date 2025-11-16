53a33dc2cda0b4db63ad62d4545ed3b6811cdb8c
Alexander Getka <alexgetka@gmail.com>
2025-11-16 18:44:08 -0500

    remove unnecessary click

--- src/viewer/utils/utils_upload_single_subject.py
+++ src/viewer/utils/utils_upload_single_subject.py
@@ -319,7 +319,7 @@ def panel_project_folder():
         )
 
         with st.container(horizontal=True, horizontal_alignment="center"):
-            if st.button("Select"):
+            if st.button("Create Project"):
                 utilss.update_project(sel_prj)
                 placeholder.markdown(f"##### 📃 `{st.session_state.prj_name}`", width='content')
 
@@ -333,14 +333,13 @@ def panel_project_folder():
                 multiple=False, color='cyan', description='Projects in output folder'
             )
             
-            with st.container(horizontal=True, horizontal_alignment="center"):
-                if st.button("Select"):
+            with st.container(horizontal=True, horizontal_alignment="center"): 
+                utilss.update_project(sel_prj)
+                placeholder.markdown(f"##### 📃 `{st.session_state.prj_name}`", width='content')
+                if sel_prj is not None:
                     utilss.update_project(sel_prj)
                     placeholder.markdown(f"##### 📃 `{st.session_state.prj_name}`", width='content')
-                    if sel_prj is not None:
-                        utilss.update_project(sel_prj)
-                        placeholder.markdown(f"##### 📃 `{st.session_state.prj_name}`", width='content')
-    
+
     if sel_opt == 'Reset project folder':
         st.warning("⚠️Are you sure you want to delete all files in the project folder? This cannot be undone.")
         flag_confirm = st.checkbox("I understand and want to delete all files in this folder")
