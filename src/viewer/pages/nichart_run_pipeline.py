a40d00588657d6550bc215bc6cfbe200b40057ba
Alexander Getka <alexgetka@gmail.com>
2025-11-16 23:08:00 -0500

    fix errbox

--- src/viewer/pages/nichart_run_pipeline.py
+++ src/viewer/pages/nichart_run_pipeline.py
@@ -152,7 +152,7 @@ def pipeline_runner_menu(enabled_pnames, sel=False):
         #process_progress_bar_slot = st.empty()
         with st.container():
             st.subheader("Pipeline Logs")
-            errbox = st.expander("Error messages")
+            errbox = st.expander("Error messages", expanded=False)
             with st.expander("View all pipeline logs"):
                 with st.container():
                     log_committed_box = st.empty()
@@ -185,8 +185,7 @@ def pipeline_runner_menu(enabled_pnames, sel=False):
             )
 
             alert_placeholder.success(f"Pipeline {pipeline_to_run} finished successfully.")
-        except Exception as err:
-            st.error()  
+        except Exception as err: 
             alert_placeholder.error(f"Pipeline {pipeline_to_run} failed with errors. Expand the log boxes for details.")
             process_status_box.update(state="error", label="Click to view error messages.", expanded=False)
             errbox.write(traceback.format_exc(), language='text')
