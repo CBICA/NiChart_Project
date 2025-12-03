901881dda5c5701970b05107354d4baaa4f7f55f
Alexander Getka <alexgetka@gmail.com>
2025-12-03 15:22:37 -0500

    debug for survey

--- src/viewer/utils/utils_survey.py
+++ src/viewer/utils/utils_survey.py
@@ -5,6 +5,7 @@ def is_survey_completed() -> bool:
     # First, check for session-local skip_survey
     if "skip_survey" in st.session_state:
         if st.session_state.skip_survey:
+            print("skip_survey in session state and true")
             return True
     # Look in the base output dir for the "survey submitted" file
     # (This occurs regardless of cloud or local)
