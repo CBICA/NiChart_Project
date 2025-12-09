b569cc741bdd5ab5dcb72ddd4d2eb7b1c1b5103e
Alexander Getka <alexgetka@gmail.com>
2025-12-09 15:42:17 -0500

    relative card size

--- src/viewer/pages/nichart_info.py
+++ src/viewer/pages/nichart_info.py
@@ -56,8 +56,8 @@ def imgfile_to_data(filepath):
 imgdir =  os.path.join(st.session_state.paths['resources'], 'images', 'nichart_logo')
 
 my_card = {
-    "width": "min(50vw, 400px)",
-    "height": "min(50vh, 400px)",
+    "width": "80vw",
+    "height": "80vh",
     "margin": "5px",
     "border-radius": "4vmin",
     "box-shadow": "0 0 10px rgba(0,0,0,0.3)",
