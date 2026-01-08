8500be1718cdf7a877ef9fcaa9643061399060fa
Alexander Getka <alexgetka@gmail.com>
2026-01-08 14:43:26 -0500

    dkgp support

--- src/viewer/utils/utils_csvparsing.py
+++ src/viewer/utils/utils_csvparsing.py
@@ -106,6 +106,18 @@ def _validate_iscn(df: pd.DataFrame, col: str, mrid_col: str) -> List[CSVIssue]:
 def _validate_batch(df: pd.DataFrame, col: str, mrid_col: str) -> List[CSVIssue]:
     return v_nonempty(df, col)
 
+@register_csv_column("ADAS_COG_13")
+def _validate_adas_cog_13(df: pd.DataFrame, col: str, mrid_col: str) -> List[CSVIssue]:
+    return v_float(df, col)
+
+@register_csv_column("MMSE")
+def _validate_mmse(df: pd.DataFrame, col: str, mrid_col: str) -> List[CSVIssue]:
+    return v_float(df, col)
+
+@register_csv_column("AD_Diagnosis")
+def _validate_ad_diagnosis(df: pd.DataFrame, col: str, mrid_col: str) -> List[CSVIssue]:
+    return v_enum(df, col, values=["CN", "MCI", "AD"])
+
 from dataclasses import asdict
 
 @dataclass
