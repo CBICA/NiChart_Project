b083cd5057db8c5419d0c5d65af2768ecae5311f
gurayerus <gurayerus@gmail.com>
2026-03-04 15:46:22 -0500

    Edits

--- src/viewer/utils/utils_session.py
+++ src/viewer/utils/utils_session.py
@@ -115,7 +115,7 @@ def init_session_vars():
     st.session_state.layout_plots = 'Sidebar'
 
     # Survey-checking code has fallbacks, don't set it.
-    #st.session_state.skip_survey = True
+    st.session_state.skip_survey = True
 
     st.session_state.workflow = None
 
