be0f535f66e6df5dc982a2c883162b96232ccecb
musa.mlinrs <musa.mlinrs@gmail.com>
2026-03-10 09:44:08 -0400

    Modified main menu items

--- src/viewer/NiChartProject.py
+++ src/viewer/NiChartProject.py
@@ -58,12 +58,12 @@ pages = {
     "Workflows": [
         st.Page("pages/nichart_ref_data.py", title="View Reference Charts"),
         st.Page("pages/nichart_single_subject.py", title="Process Single Subject"),
-        st.Page("pages/nichart_multi_subject.py", title="Multiple Subjects"),
-        st.Page("pages/nichart_results.py", title="View Results"),
-    ],    
-    "Actions": [
-        st.Page("pages/nichart_data.py", title="Data"),
-        st.Page("pages/nichart_pipelines.py", title="Pipelines"),
+        st.Page("pages/nichart_multi_subject.py", title="Process Dataset"),
+        st.Page("pages/nichart_results.py", title="View User Charts"),
+    # ],    
+    # "Actions": [
+        st.Page("pages/nichart_data.py", title="Data", visibility='hidden'),
+        st.Page("pages/nichart_pipelines.py", title="Pipelines", visibility='hidden'),
         # st.Page("pages/nichart_results.py", title="Results"),
     ],
     #"Pipelines": [
